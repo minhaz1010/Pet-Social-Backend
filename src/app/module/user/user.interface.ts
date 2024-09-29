bb20743a04090e11fced28cec42aefc5357eb3d3
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-29 12:29:57 +0600

    some changes in user interface and model

--- src/app/module/user/user.interface.ts
+++ src/app/module/user/user.interface.ts
@@ -6,13 +6,13 @@ export type TMemberShip = "regular" | "premium";
 
 export interface IUser {
   userId: string;
-  userName:string;
-  fullName:string;
+  userName: string;
+  fullName: string;
   email: string;
   imageURL?: string;
   role: TRole;
   membership: TMemberShip;
-  posts: Schema.Types.ObjectId[];
-  follower: Schema.Types.ObjectId[];
-  following: Schema.Types.ObjectId[];
+  posts?: Schema.Types.ObjectId[];
+  followers?: Schema.Types.ObjectId[];
+  followings?: Schema.Types.ObjectId[];
 }
--- src/app/module/user/user.model.ts
+++ src/app/module/user/user.model.ts
@@ -2,51 +2,51 @@ import { model, Schema } from "mongoose";
 import { IUser } from "./user.interface";
 
 const userSchema = new Schema<IUser>({
-  userId:{
-    type:String,
-    required:true,
+  userId: {
+    type: String,
+    required: true,
   },
-  userName:{
-    type:String,
-    required:true
+  userName: {
+    type: String,
+    required: true,
   },
-  fullName:{
-    type:String,
-    required:true
+  fullName: {
+    type: String,
+    required: true,
   },
-  email:{
-    type:String,
-    required:true
+  email: {
+    type: String,
+    required: true,
   },
-  imageURL:{
-    type:String,
+  imageURL: {
+    type: String,
   },
-  posts:[{
-    type:Schema.Types.ObjectId,
-    ref:"POST"
-  }],
-  follower:[
+  posts: [
     {
-      type:Schema.Types.ObjectId,
-      ref:"USER"
-    }
+      type: Schema.Types.ObjectId,
+      ref: "POST",
+    },
   ],
-  following:[
+  followers: [
     {
-      type:Schema.Types.ObjectId,
-      ref:"USER"
-    }
+      type: Schema.Types.ObjectId,
+      ref: "USER",
+    },
   ],
-  membership:{
-    type:String,
-    default:"regular"
+  followings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "USER",
+    },
+  ],
+  membership: {
+    type: String,
+    default: "regular",
   },
-  role:{
-    type:String,
-    default:"user"
-  }
+  role: {
+    type: String,
+    default: "user",
+  },
+});
 
-})
-
-export const User = model<IUser>("USER",userSchema);
-
+export const User = model<IUser>("USER", userSchema);
