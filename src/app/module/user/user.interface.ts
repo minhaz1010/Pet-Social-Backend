afad6c006082508a4126a010dacc42388c024492
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-29 12:22:46 +0600

    add user interface

---
+++ src/app/module/user/user.interface.ts
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+
+export type TRole = "user" | "admin";
+
+export type TMemberShip = "regular" | "premium";
+
+export interface IUser {
+  userId: string;
+  email: string;
+  imageURL?: string;
+  role: TRole;
+  membership: TMemberShip;
+  posts: Schema.Types.ObjectId[];
+  follower: Schema.Types.ObjectId[];
+  following: Schema.Types.ObjectId[];
+}
