8ef597addc3b43f8fd720417d864587b2ca8a64b
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-30 11:33:54 +0600

    create post controller

---
+++ src/app/module/post/post.controller.ts
@@ -0,0 +1,22 @@
+import httpStatus from "http-status";
+import catchAsyncErrors from "../../utils/catchAsyncError";
+import sendResponse from "../../utils/sendResponse";
+
+import { PostServices } from "./post.services";
+
+
+
+
+const createPost = catchAsyncErrors(async (req, res) => {
+  const result = await PostServices.createPostInDatabase(req.body,req.file?.path as string);
+  sendResponse(res, {
+    success: true,
+    result,
+    statusCode: httpStatus.CREATED,
+    message: "Successfully Created",
+  });
+});
+
+export const PostController = {
+  createPost,
+};
