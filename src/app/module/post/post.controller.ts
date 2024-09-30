0ba68539c3811c79c79222a11d2ba871d796e991
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-30 14:19:53 +0600

    add some comments

--- src/app/module/post/post.controller.ts
+++ src/app/module/post/post.controller.ts
@@ -4,6 +4,8 @@ import sendResponse from "../../utils/sendResponse";
 
 import { PostServices } from "./post.services";
 
+// NOTE: create post controller
+
 const createPost = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.createPostInDatabase(
     req.body,
@@ -17,6 +19,7 @@ const createPost = catchAsyncErrors(async (req, res) => {
   });
 });
 
+// NOTE: get all post controller
 const getAllPost = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.getAllPostInDatabase();
   sendResponse(res, {
@@ -27,6 +30,7 @@ const getAllPost = catchAsyncErrors(async (req, res) => {
   });
 });
 
+// NOTE: get single post controller by post id
 const getSinglePost = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.getSinglePostFromDatabase(
     req.params.postId,
@@ -38,7 +42,7 @@ const getSinglePost = catchAsyncErrors(async (req, res) => {
     message: "Successfully Retrieved A Post",
   });
 });
-
+// NOTE: update single post controller by post id
 const updateASinglePost = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.updateSinglePostFromDatabase(
     req.params.postId,
@@ -52,6 +56,7 @@ const updateASinglePost = catchAsyncErrors(async (req, res) => {
   });
 });
 
+// NOTE: delete single post controller by post id
 const deleteASinglePost = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.deleteSinglePostFromDatabase(
     req.params.postId,
@@ -64,6 +69,7 @@ const deleteASinglePost = catchAsyncErrors(async (req, res) => {
   });
 });
 
+// NOTE: update a free post to premium controller by post id
 const updateAPostInPremium = catchAsyncErrors(async (req, res) => {
   const result = await PostServices.updateAPostPremiumInDatabase(
     req.params.postId,
