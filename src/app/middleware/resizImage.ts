37f967dca5a45086809a5efed7e1e2d20a241e82
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-30 11:33:34 +0600

    add resizeImage middleware while uploading

---
+++ src/app/middleware/resizImage.ts
@@ -0,0 +1,21 @@
+import httpStatus from "http-status";
+import AppError from "../errors/appError";
+import catchAsyncErrors from "../utils/catchAsyncError";
+import {v2 as cloudinary} from "cloudinary"
+
+export const resizeImageOfPost = catchAsyncErrors(async(req,res,next)=>{
+  if(!req.file){
+    throw new AppError(httpStatus.BAD_REQUEST,"There is no image")
+  }
+  const result = await cloudinary.uploader.upload(req.file.path, {
+      transformation: [
+        { width: 600, height: 600, crop: "limit",quality:"auto",fetch_format:"auto" }
+      ],
+      public_id: req.file.filename,
+      overwrite: true,
+    });
+
+    req.file.path = result.secure_url;
+    req.file.filename = result.public_id;
+    next();
+})
