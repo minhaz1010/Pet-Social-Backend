4ece65b4e707913004161559bc16e926b642c8d9
minhaz1010 <minhazrahman1010@gmail.com>
2024-10-12 15:44:36 +0600

    add like a comment and dislike a comment controller adn services

--- src/app/module/comment/comment.controller.ts
+++ src/app/module/comment/comment.controller.ts
@@ -55,9 +55,36 @@ const updateASingleCommentOfAPost = catchAsyncErrors(async (req, res) => {
   });
 });
 
+const likeAComment = catchAsyncErrors(async (req, res) => {
+  const userId = req.auth.userId as string;
+  // const userId = "user_2n8JsKyG5g4roI6D3zHissu8imU";
+  const commentId = req.params.commentId;
+  const result = await CommentServices.likeAComment(commentId, userId);
+  sendResponse(res, {
+    success: true,
+    message: "Successfully Liked The Comment",
+    result,
+    statusCode: httpStatus.OK,
+  });
+});
+const dislikeAComment = catchAsyncErrors(async (req, res) => {
+  const userId = req.auth.userId as string;
+  // const userId = "user_2n8JsKyG5g4roI6D3zHissu8imU";
+  const commentId = req.params.commentId;
+  const result = await CommentServices.disLikeAComment(commentId, userId);
+  sendResponse(res, {
+    success: true,
+    message: "Successfully Disliked The Comment",
+    result,
+    statusCode: httpStatus.OK,
+  });
+});
+
 export const CommentControllers = {
   postAComment,
   getAllCommentsOfAPost,
   updateASingleCommentOfAPost,
   deleteASingleCommentsOfAPost,
+  likeAComment,
+  dislikeAComment,
 };
--- src/app/module/comment/comment.services.ts
+++ src/app/module/comment/comment.services.ts
@@ -4,18 +4,19 @@ import { Post } from "../post/post.model";
 import { IComment } from "./comment.interface";
 import { Comment } from "./comment.model";
 import { User } from "../user/user.model";
+import mongoose from "mongoose";
 // NOTE: post a comment in a  post
 const postAComment = async (postId: string, payload: Partial<IComment>) => {
   const postExist = await Post.findById(postId);
   if (!postExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "No Post Found ");
   }
-  const userData = await User.findOne({userId:payload.author});
+  const userData = await User.findOne({ userId: payload.author });
   const updatePayload = {
     ...payload,
-    post:postId,
-    author:userData?._id
-  }
+    post: postId,
+    author: userData?._id,
+  };
   const result = await Comment.create(updatePayload);
   return result;
 };
@@ -26,7 +27,7 @@ const getAllCommentsOfAPost = async (postId: string) => {
   if (!postExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "No Post Found ");
   }
-  const result = await Comment.find({ post: postId });
+  const result = await Comment.find({ post: postId }).sort("-createdAt");
   if (result.length === 0) {
     throw new AppError(
       httpStatus.BAD_REQUEST,
@@ -61,9 +62,68 @@ const updateASingleCommentOfAPost = async (
   return result;
 };
 
+const likeAComment = async (commentId: string, userId: string) => {
+  const userData = await User.findOne({ userId });
+  const commentData = await Comment.findById(commentId);
+  const userObjectId = new mongoose.Types.ObjectId(userData?._id);
+  if (!commentData) {
+    throw new AppError(httpStatus.BAD_REQUEST, "No Comment Found");
+  }
+  if (commentData.dislikedBy.includes(userObjectId)) {
+    commentData.dislikedBy = commentData.dislikedBy.filter(
+      (id) => id === userObjectId,
+    );
+    commentData.dislikes -= 1;
+  }
+  if (commentData.likedBy.includes(userObjectId)) {
+    commentData.likedBy = commentData.likedBy.filter(
+      (id) => id === userObjectId,
+    );
+    if (commentData.likes > 0) {
+      commentData.likes -= 1;
+    }
+  } else {
+    commentData.likedBy.push(userObjectId);
+    commentData.likes += 1;
+  }
+  const result = await commentData.save();
+  return result;
+};
+
+const disLikeAComment = async (commentId: string, userId: string) => {
+  const userData = await User.findOne({ userId });
+  const commentData = await Comment.findById(commentId);
+  const userObjectId = new mongoose.Types.ObjectId(userData?._id);
+  if (!commentData) {
+    throw new AppError(httpStatus.BAD_REQUEST, "No Comment Found");
+  }
+  if (commentData.likedBy.includes(userObjectId)) {
+    commentData.likedBy = commentData.likedBy.filter(
+      (id) => id === userObjectId,
+    );
+    commentData.likes -= 1;
+  }
+  if (commentData?.dislikedBy.includes(userObjectId)) {
+    commentData.dislikedBy = commentData.dislikedBy.filter(
+      (id) => id === userObjectId,
+    );
+    if (commentData.dislikedBy) {
+      commentData.dislikes -= 1;
+    }
+  } else {
+    commentData.dislikedBy.push(userObjectId);
+    commentData.dislikes += 1;
+  }
+
+  const result = await commentData.save();
+  return result;
+};
+
 export const CommentServices = {
   postAComment,
   getAllCommentsOfAPost,
   deleteASingleCommentsOfAPost,
   updateASingleCommentOfAPost,
+  likeAComment,
+  disLikeAComment,
 };
